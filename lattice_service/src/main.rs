7a978d8d66eff61a6ae5e66b37f6dd92debcb1fa
sanathNU <sanathn.upadhyaya@gmail.com>
2025-12-18 17:15:52 -0500

    added basic health endpoint

--- lattice_service/src/main.rs
+++ lattice_service/src/main.rs
@@ -1,24 +1,40 @@
-use axum::{Router, routing::get, Json};
+use axum::{debug_handler, Router, routing::get, Json};
 use std::net::SocketAddr;
+use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::net::TcpListener;
-use serde::Serialize;
 
-async fn hello_world() -> &'static str {
-    "Namaste World!"
+// === Health Check service ==
+
+#[derive(serde::Serialize)]
+struct HealthResponse {
+    status: String,
+    service: String,
+    timestamp: u64,
 }
 
-async fn return_some_json() -> Json<Value> {
-    let json = json!({"Hey":"yaall"});
-    
-    Json(json)
+#[debug_handler]
+async fn health() -> Json<HealthResponse> {
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    Json(HealthResponse {
+        status: "healthy".to_string(),
+        service: "lattice_service".to_string(),
+        timestamp,
+    })
 }
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/health", get(health));
 
     let addr = SocketAddr::from(([127,0,0,1], 8000));
     let tcp = TcpListener::bind(&addr).await.unwrap();
-
+     println!("lattice_service listening on {}", addr};
     axum::serve(tcp, router).await.unwrap();
+   
+
 }
